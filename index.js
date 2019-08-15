5df3739f909fbb85b53087528dd60916f4a195f1
unknown <abelteferany@gmail.com>
2019-08-15 11:29:09 +0300

    updated this feature

--- index.js
+++ index.js
@@ -5,9 +5,13 @@ var path = require('path')
 if (process.argv.length < 3)
 	console.log('provide repo directory')
 
+var time = process.argv[3] || 5; // seconds
+
 var repo = process.argv[2]
 var repoPath = path.join(repo, '')
 
-git(repoPath)
-	.add('.')
-	.commit('new commit')
+setTimeout(() => {
+	git(repoPath)
+	.add('.', () => console.log('added all files'))
+	.commit('updated this feature')
+}, time*1000)
