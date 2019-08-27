cb0c14b73f7baedef7fde7b183a9bc39e4842933
Abel <abelteferany@gmail.com>
2019-08-27 11:53:19 +0300

    finally, Done!. Append space at the end of target file

--- bin/index.js
+++ bin/index.js
@@ -4,16 +4,27 @@ var fs = require('fs')
 
 if (process.argv.length < 3) {
 	console.log('provide location of the repo')
+	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval=5]')
+	return
+} else if (process.argv.length < 4) {
+	console.log('provide the file be modified')
+	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval=5]')
 	return
 }
 
 var repoPath = process.argv[2]
-var interval = process.argv[3] || 5 // seconds
+var targetFile = process.argv[3]
+var interval = process.argv[4] || 5 // seconds
 
-if (fs.existsSync(repoPath)) {
+var repoExists = fs.existsSync(repoPath)
+var targetFileExists = fs.existsSync(targetFile)
+
+if (repoExists && targetFileExists) {
 	setInterval(function () {
-		commit(repoPath)
+		commit(repoPath, targetFile)
 	}, interval*1000)
-} else {
+} else if (!repoExists) {
 	console.log("repo path doesn't exist")
+} else if (!targetFileExists) {
+	console.log("target file doesn't exist")
 }
