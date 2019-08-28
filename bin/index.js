2321e2ccdc82435e00327943daccaddfe5c67305
Abel <abelteferany@gmail.com>
2019-08-28 15:27:11 +0300

    added entry point, index.js

--- bin/index.js
+++ bin/index.js
@@ -8,12 +8,12 @@ if (process.argv.length < 3) {
 	return
 } else if (process.argv.length < 4) {
 	console.log('provide the file be modified')
-	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval=5]')
+	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval = 5 seconds]')
 	return
 }
 
-var repoPath = process.argv[2]
-var targetFile = process.argv[3]
+var repoPath = process.argv[2] || '../example'
+var targetFile = process.argv[3] || '../example/app.js'
 var interval = process.argv[4] || 5 // seconds
 
 var repoExists = fs.existsSync(repoPath)
@@ -24,7 +24,7 @@ if (repoExists && targetFileExists) {
 		commit(repoPath, targetFile)
 	}, interval*1000)
 } else if (!repoExists) {
-	console.log("repo path doesn't exist")
+	console.log(`repo path '${repoPath}' doesn't exist`)
 } else if (!targetFileExists) {
-	console.log("target file doesn't exist")
+	console.log(`target file '${targetFile}' doesn't exist`)
 }
