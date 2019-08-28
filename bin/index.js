0309e7a73ac28bc0e9965e72f0c31f61fa6f7610
Abel <abelteferany@gmail.com>
2019-08-28 15:35:58 +0300

    ready, provide repo path & file to be modified, it all works fine

--- bin/index.js
+++ bin/index.js
@@ -2,21 +2,18 @@
 var commit = require('../lib')
 var fs = require('fs')
 
-print(__dirname)
+if (process.argv.length < 3) {
+	console.log('provide location of the repo')
+	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval=5]')
+	return
+} else if (process.argv.length < 4) {
+	console.log('provide the file be modified')
+	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval = 5 seconds]')
+	return
+}
 
-
-// if (process.argv.length < 3) {
-// 	console.log('provide location of the repo')
-// 	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval=5]')
-// 	return
-// } else if (process.argv.length < 4) {
-// 	console.log('provide the file be modified')
-// 	console.log('Usage: fakecommit <repo-path> <target-file> [time-interval = 5 seconds]')
-// 	return
-// }
-
-var repoPath = process.argv[2] || '../example'
-var targetFile = process.argv[3] || '../example/app.js'
+var repoPath = process.argv[2]
+var targetFile = process.argv[3]
 var interval = process.argv[4] || 5 // seconds
 
 var repoExists = fs.existsSync(repoPath)
